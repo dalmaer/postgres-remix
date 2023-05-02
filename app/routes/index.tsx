b92691607a8b367a64a96048bf3a45e107afe3a2
Dion Almaer <dion@almaer.com>
2023-05-01 21:53:56 -0600

    Bump that node for reasons

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       <Link
-        to="https://vercel.com/templates/remix/postgres-starters"
+        to="https://vercel.com/templates/remix/postgres-starter"
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
         <p>Deploy your own to Vercel</p>
